778375db67d719c0526b1acdd73e5c5547514bb9
Denilson Sá Maia <denilsonsa@gmail.com>
2017-07-30 23:46:21 +0200

    Initial version is ready!
    I've started writing this project yesterday.
    
    Now it is feature-complete, although lacking polish. And it also has a
    nice README.

---
+++ README.md
@@ -0,0 +1,114 @@
+Calculator: The Game — Solver
+=============================
+
+[![Donate using PayPal](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=denilsonsa%40gmail%2ecom&lc=US&item_name=Denilson&item_number=calculator-solver&currency_code=BRL) [![Flattr this project](https://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=denilsonsa&url=https%3A%2F%2Fgithub.com%2Fdenilsonsa%2Fcalculator-solver&title=Calculator+solver&description=Solver+for+Calculator+The+Game.&tags=github&category=software)
+
+This is a tool to help solving levels from [Calculator: The Game][game], (a game [for Android][android] and [for iOS][ios]).
+
+The game consists of a calculator with only a few buttons for very specific operations, and a limited number of moves (button presses) to get to a specific goal number.
+
+This solver explores all possible numbers that can be reached given the initial conditions. This is done by a [breadth-first search][bfs] that builds a [directed][] [graph][] of numbers.
+
+How to use this solver?
+-----------------------
+
+1. Input the initial game state: number of moves and the initial value.
+2. Input all possible operations, one per line.
+3. Optionally, input the goal.
+4. Press the button.
+5. Enjoy the results.
+
+### What are the operations recognized by the solver?
+
+These are all the operations recognized by the solver. Some of them support multiple syntaxes for the the same results.
+
+* Addition: `+1`, `+42`
+* Subtraction: `-1`, `-42`
+* Multiplication: `x2`, `X2`, `*2`, `x-3`, `X-3`, `*-3`
+* Division: `/2`, `/-3`
+* Typing numbers: `2`, `42`
+* Backspace: `<<`
+* Substitution: `1=>2`, `42=>13`
+* Exponentiation: `x**2`, `x^2`, `x²`, `**3`, `^3`, `³`
+* Signal: `+/-`, `-/+`, `+-`, `-+`, `±`, `∓`
+
+ELI5 How does this solver work?
+-------------------------------
+
+(*ELI5* means *Explain Like I'm Five*.)
+
+In order to understand how this solver work, let's look at one sample level, and let's learn how you can solve any level yourself, using just a piece of paper.
+
+    Moves: 2
+    Goal: 6
+    Initial number: 0
+    Operations: +2 +4
+
+The game begins with `2 moves` and the number `0`. That's the initial state, and we have to put it on paper. Write the number `0` somewhere in the paper, and write `2 moves` near it. You may even draw a circle around those two numbers, or write them as `(0,2)`.
+
+We have two possible operations: `+2` and `+4`. Which one leads to the goal? We don't know yet, so we are going to explore both of them. `0 +2` is `2`, but since we spent one move, we now have `1 move` left. So put it on paper: `2` with `1 move` (from now on, I'll just use the `(2,1)` notation because it is shorter). Also draw an arrow from `(0,2)` towards `(2,1)`, and write the operation `+2` at the arrow.
+
+We have to do the same for the second operation: `0 +4` is `4`, with only `1 move` left, so write down `(4,1)` and draw an arrow from the inital state to this new state. We should have something like this on paper:
+
+    (0,2)
+      ├── +2 ──> (2,1)
+      └── +4 ──> (4,1)
+
+Since there are only two operations, we've explored all possible game states that we can get after we do one move. How do we continue from there? It's simple, we repeat the exact same steps, but starting from `(2,1)` and `(4,1)`. Let's continue…
+
+Applying `+2` to `(2,1)` leads to `2 +2 = 4` with `0 moves` left. So we can write down `(4,0)` and draw an arrow.
+
+    (0,2)
+      ├── +2 ──> (2,1)
+      │            └── +2 ──> (4,0)
+      └── +4 ──> (4,1)
+
+But, if we are paying attention, we can notice that we already know how to get to `4`, and we even know how to get there in fewer moves. So we don't need to write `(4,0)`. Or we can cross it to mark that we don't want to explore it anymore.
+
+Let's continue with the next operation… Applying `+4` to `(2,1)` results in `(6,0)`. `6` already is our goal, so we don't need to go any further. However, for the sake of the explanation, I will not stop.
+
+    (0,2)
+      ├── +2 ──> (2,1)
+      │            ├── +2 ──> (4,0) ✗
+      │            └── +4 ──> (6,0)
+      └── +4 ──> (4,1)
+
+We've applied all the operations to `(2,1)`, it's time to move to `(4,1)` and repeat the same steps. Applying `+2` to `(4,1)` results in `(6,0)`, which is the exact same state (i.e. number and moves) that we arrived earlier. Instead of writing `(6,0)` twice, we can just draw an arrow to the previous one.
+
+    (0,2)
+      ├── +2 ──> (2,1)
+      │            ├── +2 ──> (4,0) ✗
+      │            └── +4 ──> (6,0)
+      └── +4 ──> (4,1)          ↑
+                   └── +2 ──────┘
+
+What does it mean? It means we have multiple paths to arrive at the state `(6,0)`. Since `6` is the goal, it means we have two solutions for getting to the goal: `+2 +4` and `+4 +2`.
+
+But we aren't finished with `(4,1)` yet. We still have to apply `+4`, which results in `(8,0)`.
+
+    (0,2)
+      ├── +2 ──> (2,1)
+      │            ├── +2 ──> (4,0) ✗
+      │            └── +4 ──> (6,0)
+      └── +4 ──> (4,1)          ↑
+                   ├── +2 ──────┘
+                   └── +4 ──> (8,0)
+
+Now, we've explored all operations for all game states that have only one move left. This means we've discovered all possible game states for zero moves left. What's next? Well, since the new game states we've discovered don't have any moves left, we can stop the process. Otherwise, we would repeat the same steps (applying all operations) for all (intermediate) game states until we get to all possible final states (i.e. no moves left).
+
+And what's the result of all this work? We get a list of all possible outcomes of a game. We get a list of all possible numbers we can reach given the inital conditions. And we can trace a path of all operations needed to reach any number.
+
+TODO
+----
+
+This is the list of dev tasks that are planned (but with no date):
+
+* Proper error handling and error reporting (both error messages and CSS styling for wrong input).
+* Better layout.
+
+[game]: http://www.simplemachine.co/game/calculator-the-game/
+[android]: https://play.google.com/store/apps/details?id=com.sm.calculateme
+[ios]: https://itunes.apple.com/us/app/calculator-the-game/id1243055750
+[bfs]: https://en.wikipedia.org/wiki/Breadth-first_search
+[graph]: https://en.wikipedia.org/wiki/Graph_(abstract_data_type)
+[directed]: https://en.wikipedia.org/wiki/Directed_graph
---
+++ solver.js
@@ -0,0 +1,297 @@
+'use strict';
+
+function map_get_or_set(map, key, default_value) {
+	if (map.has(key)) return map.get(key);
+	map.set(key, default_value);
+	return default_value;
+}
+
+function CalculatorButton(text, type, exec_fn) {
+	this.text = text;
+	this.type = type;
+	this.exec = exec_fn;
+}
+
+function Graph(start_node) {
+	this.node_list = [];
+	this.node_map = new Map();
+	this.nodes_by_moves = new Map();
+	this.start_node = start_node;
+	this.add_node(start_node);
+}
+
+Graph.prototype.get_node = function(value) {
+	// Returns undefined if node is not in the Map.
+	return this.node_map.get(value);
+}
+
+Graph.prototype.add_node = function(node) {
+	if (this.node_map.has(node.value)) {
+		throw 'Node is already in the graph.';
+	}
+	this.node_list.push(node);
+	this.node_map.set(node.value, node);
+	map_get_or_set(this.nodes_by_moves, node.moves_left, []).push(node);
+}
+
+function Node(value, moves_left) {
+	this.value = value;
+	this.moves_left = moves_left;
+	this.edges = [];
+	this.is_goal = false;
+}
+
+function Edge(dest, label, is_forward) {
+	this.dest = dest;
+	this.label = label;
+	this.is_forward = is_forward;
+}
+
+function sorted(array, sort_key_fn) {
+	return array.concat().sort(function(a, b) {
+		var _a = sort_key_fn(a);
+		var _b = sort_key_fn(b);
+		if (_a < _b) return -1;
+		if (_a > _b) return  1;
+		return 0;
+	});
+}
+
+
+function parse_single_operation(str) {
+	var patterns = [
+		{
+			're': /^([-+])\s*([0-9]+)$/,
+			'name': 'addsub',
+			'parse': function(match, signal, number) {
+				var x = parseInt(signal + '' + number, 10);
+				return function(value) { return value + x; };
+			},
+		},
+		{
+			're': /^([xX*\/])\s*([-+]?)\s*([0-9]+)$/,
+			'name': 'muldiv',
+			'parse': function(match, op, signal, number) {
+				var x = parseInt(signal + '' + number, 10);
+				if (/[xX*]/.test(op)) {
+					return function(value) { return value * x; };
+				} else if (/[\/]/.test(op)) {
+					return function(value) { return value / x; };
+				} else {
+					throw 'Unrecognized "op". This should never happen.';
+				}
+			},
+		},
+		{
+			're': /^([0-9]+)$/,
+			'name': 'append',
+			'parse': function(match, digits) {
+				return function(value) { return parseInt(value + '' + digits, 10); };
+			},
+		},
+		{
+			're': /^(<<?)$/,
+			'name': 'backspace',
+			'parse': function(match, backspace) {
+				// This works for both positive and negative numbers because:
+				//  -97 % 10 = -7
+				//  -97 - (-7) = -90
+				return function(value) { return (value - (value % 10)) / 10; };
+			},
+		},
+		{
+			're': /^([0-9]+)\s*=>\s*([0-9]+)$/,
+			'name': 'replace',
+			'parse': function(match, from, to) {
+				var re = new RegExp(from, 'g');
+				return function(value) { return parseInt((value + '').replace(re, to), 10); };
+			},
+		},
+		{
+			're': /^x?\s*(\^|\*\*)\s*([0-9])$/,
+			'name': 'power',
+			'parse': function(match, operator, exp) {
+				var x = parseInt(exp, 10);
+				return function(value) { return Math.pow(value, x); };
+			},
+		},
+		{
+			're': /^x?\s*([²³⁴⁵⁶⁷⁸⁹])$/,
+			'name': 'power',
+			'parse': function(match, exp) {
+				var x = {
+					'²': 2,
+					'³': 3,
+					'⁴': 4,
+					'⁵': 5,
+					'⁶': 6,
+					'⁷': 7,
+					'⁸': 8,
+					'⁹': 9,
+				}[exp];
+				return function(value) { return Math.pow(value, x); };
+			},
+		},
+		{
+			're': /^(\+\s*\/?\s*-|-\s*\/?\s*\+|±|∓)$/,
+			'name': 'signal',
+			'parse': function(match) {
+				return function(value) { return -value; };
+			},
+		},
+	];
+
+	str = str.trim();
+	for (let pat of patterns) {
+		let match = pat.re.exec(str);
+		if (match) {
+			return new CalculatorButton(str, pat.name, pat.parse.apply(null, match));
+		}
+	}
+	// TODO: Some error handling.
+}
+
+function parse_operations(str) {
+	var lines = str.split(
+		/[\r\n]+/g
+	).map(
+		line => line.trim()
+	).filter(
+		line => line.length > 0
+	);
+	var buttons = lines.map(
+		line => parse_single_operation(line)
+	);
+	return buttons;
+}
+
+function breadth_first_search(start_node, operations) {
+	var g = new Graph(start_node);
+	var queue = [start_node];
+	while (queue.length > 0) {
+		let node = queue.shift();
+		if (node.moves_left <= 0) continue;  // or break
+		for (let op of operations) {
+			let new_value = op.exec(node.value);
+			if (!Number.isInteger(new_value)) continue;  // Only integers. TODO: make this configurable.
+			if (!g.get_node(new_value)) {
+				let new_node = new Node(new_value, node.moves_left - 1);
+				g.add_node(new_node);
+				queue.push(new_node);
+			}
+			node.edges.push(new Edge(
+				new_value,
+				op.text,
+				(g.get_node(new_value).moves_left < node.moves_left)
+			));
+		}
+	}
+	return g;
+}
+
+function explored_nodes_to_dot(graph) {
+	var dot = 'digraph "" {\ngraph [rankdir=LR,nodesep=0.03125];\nnode [shape=box,style=rounded,margin=0];\n';
+	for (let node of graph.node_list) {
+		for (let edge of node.edges) {
+			if (!edge.is_forward) continue;  // Only forward edges. TODO: make this configurable.
+			dot += `"${node.value}" -> "${edge.dest}" [label="${edge.label}"];\n`;
+		}
+	}
+	for (let [moves_left, nodes] of graph.nodes_by_moves) {
+		dot += '{ rank=same; ';
+		for (let node of nodes) {
+			dot += `"${node.value}"; `;
+		}
+		dot += '}\n';
+	}
+	dot += '}';
+	return dot;
+}
+
+function createSubtreeElement(parent_text, parent_tooltip, is_goal, children_elements) {
+	var subtree = document.createElement('div');
+	subtree.classList.add('subtree');
+
+	if (is_goal) {
+		subtree.classList.add('goal');
+	}
+
+	var parent = document.createElement('div');
+	parent.classList.add('parent');
+	parent.textContent = parent_text;
+	parent.setAttribute('title', parent_tooltip);
+	subtree.appendChild(parent);
+
+	var children = document.createElement('div');
+	children.classList.add('children');
+	subtree.appendChild(children);
+
+	if (!children_elements || children_elements.length == 0) {
+		// No children.
+		subtree.classList.add('leaf');
+	} else {
+		// With children.
+		if (children_elements.every(elem => elem.classList.contains('leaf'))) {
+			subtree.classList.add('before_leaves');
+		}
+		if (children_elements.some(elem => elem.classList.contains('goal'))) {
+			subtree.classList.add('goal');
+		}
+		for (let child of children_elements) {
+			children.appendChild(child);
+		}
+	}
+	return subtree;
+}
+
+function createTreeFromGraph(graph) {
+	function recursive_create_subtree_for_node(node, tooltip) {
+		var edges = sorted(node.edges.filter(edge => edge.is_forward), edge => edge.dest);
+		var children_elements = edges.map(edge => recursive_create_subtree_for_node(graph.get_node(edge.dest), edge.label));
+		return createSubtreeElement(node.value, tooltip, node.is_goal, children_elements);
+	}
+
+	return recursive_create_subtree_for_node(graph.start_node, '');
+}
+
+function do_it() {
+	var input_operations = document.getElementById('input_operations').value;
+	var buttons = parse_operations(input_operations);
+	// for (let b of buttons) {
+	// 	console.log(b);
+	// }
+	var start_node = new Node(
+		document.getElementById('input_start').valueAsNumber,
+		document.getElementById('input_moves').valueAsNumber
+	);
+
+	var graph = breadth_first_search(start_node, buttons);
+
+	var input_goal = document.getElementById('input_goal').valueAsNumber;
+	var goal_node = graph.get_node(input_goal);
+	if (goal_node) {
+		goal_node.is_goal = true;
+	}
+
+	document.getElementById('debug_graphviz').value = explored_nodes_to_dot(graph);
+	// console.log(nodes);
+	var solution_tree = document.getElementById('solution_tree');
+	solution_tree.innerHTML = '';
+	solution_tree.append(createTreeFromGraph(graph));
+}
+
+function toggle_collapse(ev) {
+	if (ev.target.classList.contains('parent')) {
+		var subtree = ev.target.parentNode;
+		if (subtree.classList.contains('subtree') && !subtree.classList.contains('leaf')) {
+			subtree.classList.toggle('collapsed');
+		}
+	}
+}
+
+function init() {
+	var solution_tree = document.getElementById('solution_tree');
+	solution_tree.addEventListener('click', toggle_collapse);
+}
+
+init();
