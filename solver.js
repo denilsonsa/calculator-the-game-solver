88e48188a8ece29bd36a708b3c451a30f6869c0a
Jason Khanlar <jason.khanlar@gmail.com>
2019-06-16 01:09:41 -0400

    fix spacing

--- solver.js
+++ solver.js
@@ -211,7 +211,7 @@ function parse_single_operation(str) {
 			're': /^(\+\/-|flip)$/i,
 			'name': 'flip',
 			'parse': function(match) {
-                return function(value) { return value * -1; };
+				return function(value) { return value * -1; };
 			},
 		},
 	];
